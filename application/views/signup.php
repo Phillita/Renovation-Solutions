4fcd849c8e0f4e1b19f63ae705e5c95a77aab397
Billy Cai <b.cai.london@gmail.com>
2011-10-26 22:30:50 -0400

    fixed homepage css

--- application/views/signup.php
+++ application/views/signup.php
@@ -1,36 +1,34 @@
-<div class="wrap">
+<div id="homesignup">
     <form id="signup_form" action="sign_up" method="post">
         <fieldset class="floatright">
             <Legend class="legendtitle">Sign Up</legend>
             <div id="content">
                 Register here or start a free trial!
             </div>
-            <table id="attributes" class="table">
-                <tr>
-                    <td class="label">
-                        Username
-                    </td>
-                    <td>
-                        <input type="text" id="username" name="username"/>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="label">
-                        password
-                    </td>
-                    <td>
-                        <input type="text" id="password" name="password"/>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="buttons">
-                        <button id="sign_up_button" type="submit" class="inputbtn" name="sign_up_button">Sign up</button>
-                    </td>
-                    <td>
-                        <button id="free_trial_sign_up_button" type="submit" class="inputbtn" name="free_trial_sign_up_button">Free Trial</button>
-                    </td>
-                </tr>
-            </table>
+            <div class="wrap">
+                <table id="attributes" class="table">
+                    <tr>
+                        <td class="label">
+                            Username
+                        </td>
+                        <td>
+                            <input type="text" id="username" name="username"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="label">
+                            password
+                        </td>
+                        <td>
+                            <input type="text" id="password" name="password"/>
+                        </td>
+                    </tr>
+                </table> 
+            </div>
+            <div class="buttons" style="margin-top: 0px">
+                <button id="sign_up_button" type="submit" class="inputbtn" name="sign_up_button">Sign up</button>
+                <button id="free_trial_sign_up_button" type="submit" class="inputbtn" name="free_trial_sign_up_button">Free Trial</button>
+            </div>           
         </fieldset>
     </form>
 </div>
