b183f6f9795fa1572e361ea612fb405ee6e729e6
Tayler Phillips <swiftstar20@hotmail.com>
2011-11-01 21:36:11 -0400

    first layout

---
+++ application/views/mainlayout.php
@@ -0,0 +1,14 @@
+<script>
+    $(document).ready(function () {
+        $('#body').layout({ applyDefaultStyles: true });
+    });
+</script>
+    
+<div id="body" style="height:600px">
+    <div class="ui-layout-center">Center</div>
+    <div class="ui-layout-north">North</div>
+    <div class="ui-layout-south">South</div>
+    <div class="ui-layout-east">East</div>
+    <div class="ui-layout-west">West2</div>
+    <div class="ui-layout-west">West</div>
+</div>
